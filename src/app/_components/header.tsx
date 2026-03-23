94bac1c46399dcd6244f996be368fc644e0cbfa5
Isaque de Sousa <isaque.error404@gmail.com>
2026-03-23 14:46:22 -0300

    feat: refactor Header component structure and update image dimensions

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,28 +1,34 @@
 import { MenuIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { HeaderNav } from "./header-nav"
 import SidebarSheet from "./sidebar-sheet"
 import { Button } from "./ui/button"
-import { Card, CardContent } from "./ui/card"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 
 export function Header() {
   return (
-    <Card>
-      <CardContent className="flex flex-row items-center justify-between p-5">
+    <header className="flex flex-col border-b border-border bg-background">
+      <div className="flex items-center justify-between px-5 py-6 md:px-32">
         <Link href="/">
-          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={130} />
         </Link>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button size="icon" variant="outline">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SidebarSheet />
-        </Sheet>
-      </CardContent>
-    </Card>
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <MenuIcon size={20} />
+              </Button>
+            </SheetTrigger>
+            <SidebarSheet />
+          </Sheet>
+        </div>
+
+        <div className="hidden md:flex">
+          <HeaderNav />
+        </div>
+      </div>
+    </header>
   )
 }
